a3fe12fb0627b0fa898502439d3a6e65f5162798
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-30 00:03:11 +0800

    Delete lib directory

--- lib/frilans_finans_api/nil_event_logger.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module FrilansFinansAPI
-  class NilEventLogger
-    def request_event(*); end
-  end
-end
--- lib/frilans_finans_api/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module FrilansFinansAPI
-  VERSION = '0.4.0'.freeze
-end
